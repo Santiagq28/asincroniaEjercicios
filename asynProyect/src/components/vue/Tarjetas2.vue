d4ea3047d417107ee82902dda7e5738d93e2f614
Santiago Guevara <saguevaraq@ufpso.edu.co>
2024-06-18 21:23:12 -0500

    Update 2

---
+++ asynProyect/src/components/vue/Tarjetas2.vue
@@ -0,0 +1,49 @@
+<template>
+    <div v-if="data2" class="flex bg-slate-400 w-[60vh] h-36 p-4 m-4"> 
+      <div 
+        v-if="data2[0].characterDirection === 'Left'"
+        class="w-[50%] h-auto bg-top bg-cover rounded-full"
+        :style="{ backgroundImage: `url(${data2[0].image})` }"
+      > 
+      </div>
+      <div class="grid">
+        <h2>{{ data2[0].character }}</h2>
+        <p class="text-xs">{{ data2[0].quote }}</p>
+      </div>
+      <div 
+        v-if="data2[0].characterDirection === 'Right'"
+        class="w-[50%] h-auto bg-top bg-cover rounded-full"
+        :style="{ backgroundImage: `url(${data2[0].image})` }"
+      > 
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref, onMounted } from 'vue';
+  
+  const data2 = ref(null);
+  
+  async function getData() {
+    const response = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes?count=1');
+    const dataJSON = await response.json();
+    console.log('API Response:', dataJSON);
+    data2.value = dataJSON;
+  }
+  
+  onMounted(() => {
+    console.log('Component mounted');
+    getData();
+  });
+  </script>
+  
+  <style scoped>
+  .imagen {
+    width: 100px;
+    height: 100px;
+    background-color: aqua;
+    background-size: cover;
+    background-position: center;
+  }
+  </style>
+  
